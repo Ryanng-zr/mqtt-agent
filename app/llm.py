1286e1cf77e64aa120b93ec8847c05baf5f4de81
Ryanng-zr <e0544229@u.nus.edu>
2025-12-05 10:59:20 +0800

    remove api key

--- app/llm.py
+++ app/llm.py
@@ -1,9 +1,11 @@
 import google.generativeai as genai
 import os
+from dotenv import load_dotenv
+load_dotenv()
 if "GEMINI_API_KEY" not in os.environ:
     raise RuntimeError("Please set GEMINI_API_KEY.")
 
-genai.configure(api_key="AIzaSyAAopp9JM2R3SWfJcjN9hbUDGSdZx236i4")
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
 # change this to use the create_agent
 
